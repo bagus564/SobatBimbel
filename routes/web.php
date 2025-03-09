6d4e866930a33f66a363df302fceae8ee48f0bf9
bagus564 <bagustri0823@gmail.com>
2025-03-09 13:46:00 +0700

    UPDATE FILE README.MD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//    return view('welcome');
+// });
+
+route::get('/', [HomeController::class, 'index']);
+route::get('/contact', [HomeController::class, 'contact']);
